2c332299e58274f62b12318bcb38fc9180efc4b8
Emilio Sala <emilio_k88@hotmail.com>
2026-02-19 13:21:29 +0100

    add schema tool definition to function_call

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+
+SYSTEM_PROMPT = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info],
+)
 
 def main():
     load_dotenv()
@@ -13,16 +30,30 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model = "gemini-2.5-flash", contents=messages)
+
+    config = types.GenerateContentConfig(
+        system_instruction=SYSTEM_PROMPT,
+        temperature=0,
+        tools=[available_functions]
+    )
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents=messages,
+        config=config
+    )
 
     if response is None or response.usage_metadata is None:
         return
-
     if (args.verbose):
         print(f"User prompt: \n{args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response: \n{response.text}")
+    print(f"{response.text}")
+    if response.function_calls:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+
+
 
 
 if __name__ == "__main__":
